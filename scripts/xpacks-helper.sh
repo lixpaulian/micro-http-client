83ea9dccffeff67dd6b17b8d6e5bc175d2ea7f74
Lix N. Paulian <lix@paulian.net>
2023-01-21 19:19:17 +0100

    Scripts directory added to the repository.

---
+++ scripts/xpacks-helper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#set -euo pipefail
+#IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Bash helper script used in project generate.sh scripts.
+# -----------------------------------------------------------------------------
+
+do_add_micro_http_client_xpack() {
+  local pack_name='micro_http_client'
+  do_tell_xpack "${pack_name}-xpack"
+
+  do_select_pack_folder "lix/${pack_name}.git"
+
+  do_prepare_dest "${pack_name}/src"
+  do_add_content "${pack_folder}/src"/* 
+}
+
+# -----------------------------------------------------------------------------
