769e59e13dfd0acbe61199f9c4e5144f291e7464
Lix N. Paulian <lix@paulian.net>
2023-02-12 11:32:16 +0100

    Fixed the xpack name.

--- scripts/xpacks-helper.sh
+++ scripts/xpacks-helper.sh
@@ -7,13 +7,13 @@
 # -----------------------------------------------------------------------------
 
 do_add_micro_http_client_xpack() {
-  local pack_name='micro_http_client'
+  local pack_name='micro-http-client'
   do_tell_xpack "${pack_name}-xpack"
 
   do_select_pack_folder "lix/${pack_name}.git"
 
   do_prepare_dest "${pack_name}/src"
-  do_add_content "${pack_folder}/src"/* 
+  do_add_content "${pack_folder}/src"/*
 }
 
 # -----------------------------------------------------------------------------
